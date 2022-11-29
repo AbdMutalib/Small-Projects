206032877c7c486e167efb73752b8e9c2f8692d0
Abdul Mutalib <abdulmutalib00100@gmail.com>
2022-11-29 17:36:35 +0100

    Hmm

---
+++ Text Repeater.py
@@ -0,0 +1,8 @@
+print("Text Repeater : By lancy.py")
+user_text = input("Enter Text To Be Repeated : ")
+repeat_num = int(input("Enter The Number Of Times To Be Repeated : "))
+
+print(" Here You Go :) ")
+print("")
+for x in range (int(repeat_num)):
+    print(user_text)
---
+++ app.py
@@ -0,0 +1,6 @@
+prices = [10, 20, 30]
+
+total = 0
+for lancy in prices:
+    total = total + prices
+print("Total")
